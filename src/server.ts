9039797421e20a01fe159081c06e80167ef2713e
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-25 14:26:44 -0300

    Fix bug on promise server

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,49 @@
 import fastify from "fastify";
 import { prisma } from "./lib/prisma";
+import crypto from 'node:crypto'
 
 
 
 const app = fastify()
 
-app.get('/corretores', (req, res) => {
+app.get('/corretores', async (req, res) => {
 
-  const corretores = prisma.corretor.findMany()
+  const corretores = await prisma.corretor.findMany()
+  console.log('olámundo')
 
-  res.send(corretores)
+  return res.send(corretores)
 })
+
+app.get('/propostas', async (req, res) => {
+
+  const propostas = await prisma.proposta.findMany()
+
+  return res.send(propostas + 'ola mundo')
+})
+
+app.get('/clientes', async (req, res) => {
+
+  const clientes = await prisma.client.findMany()
+
+  res.send(clientes)
+})
+
+app.post('/corretores', async (req, res) => {
+  const {name, username, isAdmin}: any = req.body
+
+  const newCorretor = await prisma.corretor.create({
+    data:{
+      name: name,
+      username: username,
+      id: crypto.randomUUID(),
+      isAdmin: isAdmin,
+    }
+  })
+  console.log(newCorretor)
+  return res.send(newCorretor)
+})
+
+
 
 app.listen({
   port: 3333
