ac4dae7d203ba64e45923c3bcaa2e2e5045e1bb9
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-27 15:32:46 -0300

    Create route FindCorretorByUserName, findAllCorretores

--- src/routes/corretores.ts
+++ src/routes/corretores.ts
@@ -1,11 +1,74 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
+import * as z from 'zod'
+import { randomUUID } from 'crypto'
+
+const userCorretorSchema = z.object({
+  name: z.string(),
+  username: z.string(),
+  isAdmin: z.boolean(),
+})
+type UserCorretor = z.infer<typeof userCorretorSchema>
 
 export async function corretoresRoutes(app: FastifyInstance) {
+  // Capturar corretores
   app.get('/corretores', async (req, res) => {
     const corretores = await prisma.corretor.findMany()
     console.log('olámundo')
 
     return corretores
+  })
+
+  app.get('/corretores/:username', async (req, res) => {
+    const getCorretoresParamsSchema = z.object({
+      username: z.string(),
+    })
+
+    const { username } = getCorretoresParamsSchema.parse(req.params)
+
+    const corretor = await prisma.corretor.findFirst({
+      where: { username },
+    })
+
+    if (corretor) {
+      console.log('corretor achado!')
+      return res.send(corretor)
+    } else {
+      return res
+        .status(404)
+        .send({ message: 'Corretor não encontrado em nosso sistema' })
+    }
+  })
+  // Cadastro de corretor
+  app.post('/corretores', async (req, reply) => {
+    try {
+      const userCorretorData: UserCorretor = userCorretorSchema.parse(req.body)
+
+      const { isAdmin, name, username } = userCorretorData
+
+      const novoCorretor = await prisma.corretor.create({
+        data: {
+          id: randomUUID(),
+          isAdmin,
+          name,
+          username,
+        },
+      })
+
+      reply.send(novoCorretor)
+    } catch (error) {
+      if (error.code === 'P2002' && error.meta.target.includes('username')) {
+        console.log(
+          'O nome de usuário já está em uso. Por favor, tente novamente com um nome de usuário diferente.',
+        )
+      } else {
+        console.log('Ocorreu um erro ao criar o corretor:', error)
+      }
+      reply
+        .status(500)
+        .send(
+          'Ocorreu um erro ao criar o corretor. Por favor, tente novamente.',
+        )
+    }
   })
 }
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,12 @@ import { corretoresRoutes } from './routes/corretores'
 
 const app = fastify()
 
-app.register(corretoresRoutes)
+// Register server de plugin no fastify para poder consumir dados de outra parte do código...
+// é possível passar um objeto de configurações, como segundo argumento, que aceita o prefix:
+// ele serve para deixar um prefixo na rota, /users/corretores : exemplo
+app.register(corretoresRoutes, {
+  prefix: 'users',
+})
 
 app.get('/propostas', async (req, res) => {
   const propostas = await prisma.proposta.findMany()
